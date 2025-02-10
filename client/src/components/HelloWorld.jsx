c7a8013e481eeaeec0108e16e3ba28e1365696c8
Amy <amynguyen0132@gmail.com>
2025-02-10 16:11:01 -0500

    Add skeleton of project, hello world!

---
+++ client/src/components/HelloWorld.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+/**
+ * 
+ * @returns - Hello world component with nav bars.
+ */
+export default function HelloWorld() {
+  return (
+    <div>
+      <h3>Hello World</h3>
+    </div>
+  );
+}
---
+++ client/src/main.jsx
@@ -0,0 +1,25 @@
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { StrictMode } from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+import ErrorPage from './error-page.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    errorElement: <ErrorPage/>
+  }
+]);
+
+createRoot(document.getElementById('root')).
+  render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  );
