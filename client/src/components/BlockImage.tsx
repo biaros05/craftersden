f785a2de4bc785e411d198fe35a9c57d99a24fe4
MM1400 <marin.melentii@gmail.com>
2025-02-11 12:46:58 -0500

    Add ActionIcon to BlockImage

--- client/src/components/BlockImage.tsx
+++ client/src/components/BlockImage.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import '../App.css';
-import {Image} from '@mantine/core';
+import {Image, ActionIcon} from '@mantine/core';
+import { IconPlus } from '@tabler/icons-react';
 
 export default function BlockImage({src = null, alt = ''}) {
 
   return (
     <div className="block-image">
+    <ActionIcon
+      size="xs"
+    >
+      <IconPlus />
+    </ActionIcon>
       <Image
         src={src}
         h={200}
--- client/src/components/BlockScrollArea.tsx
+++ client/src/components/BlockScrollArea.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { ScrollArea, SimpleGrid } from '@mantine/core';
 import BlockImage from './BlockImage';
 
-export function BlockScrollArea({blockList}) {
+export default function BlockScrollArea({blockList}) {
   return (
     <ScrollArea h={250} type="always" scrollbarSize={12}>
       <SimpleGrid cols={4} spacing="sm">
