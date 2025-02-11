9be9ac792506352a1d072687331d247998fc9f2a
Amy <amynguyen0132@gmail.com>
2025-02-11 10:19:12 -0500

    Create empty CraftersDen component.

---
+++ client/src/components/CraftersDen.jsx
@@ -0,0 +1,8 @@
+export default function CraftersDen(){
+    return (
+        <div>
+            <h1>Crafters Den</h1>
+            <p>Here you can find all the crafters in the server and their contact information.</p>
+        </div>
+    )
+}
