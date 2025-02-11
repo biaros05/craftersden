1aef1ca92662cb2d9c7592d172923f84666f7ec9
MM1400 <marin.melentii@gmail.com>
2025-02-11 12:34:01 -0500

    ModifyBlockScrollArea to be a SimpleGrid

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,22 @@ import React from 'react';
 import './App.css';
 import HelloWorld from './components/HelloWorld';
 import BlockImage from './components/BlockImage';
+import BlockScrollArea from './components/BlockScrollArea';
+
+const blockList = [
+  { name: 'grass', src: 'https://www.filterforge.com/filters/11635.jpg' },
+  { name: 'dirt', src: 'https://www.filterforge.com/filters/11636.jpg' },
+  { name: 'stone', src: 'https://www.filterforge.com/filters/11637.jpg' },
+  { name: 'sand', src: 'https://www.filterforge.com/filters/11638.jpg' },
+  { name: 'water', src: 'https://www.filterforge.com/filters/11639.jpg' },
+  { name: 'lava', src: 'https://www.filterforge.com/filters/11640.jpg' },
+  { name: 'wood', src: 'https://www.filterforge.com/filters/11641.jpg' },
+  { name: 'leaves', src: 'https://www.filterforge.com/filters/11642.jpg' },
+  { name: 'glass', src: 'https://www.filterforge.com/filters/11643.jpg' },
+  { name: 'brick', src: 'https://www.filterforge.com/filters/11644.jpg' }
+];
+
+
 
 function App() {
   
@@ -10,6 +26,7 @@ function App() {
       <main>
         <section id="main-app">
           <HelloWorld></HelloWorld>
+          <BlockScrollArea blockList={blockList}/>
         </section>
       </main>
     </>
--- client/src/components/BlockScrollArea.tsx
+++ client/src/components/BlockScrollArea.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { ScrollArea, Grid } from '@mantine/core';
+import { ScrollArea, SimpleGrid } from '@mantine/core';
+import BlockImage from './BlockImage';
 
-function BlockScrollArea({blockList}) {
+export function BlockScrollArea({blockList}) {
   return (
     <ScrollArea h={250} type="always" scrollbarSize={12}>
+      <SimpleGrid cols={4} spacing="sm">
       {blockList.map((block, index) => 
-        <Grid>
-          <Grid.Col span={3} key={index}> block </Grid.Col>
-        </Grid>
+          <BlockImage src={block.src} alt={block.name} key={index}/>
       )}
+      </SimpleGrid>
     </ScrollArea>
   );
 }
