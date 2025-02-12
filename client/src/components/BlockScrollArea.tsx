673b298c3c66c9a7daa413e042a541313cccfabf
MM1400 <marin.melentii@gmail.com>
2025-02-12 15:25:56 -0500

    Refactor Autocomplete in BlockScrollArea into new component BlockSearchBar

--- client/src/components/BlockScrollArea.tsx
+++ client/src/components/BlockScrollArea.tsx
@@ -1,30 +1,13 @@
 import React, {useState} from 'react';
 import { ScrollArea, SimpleGrid, Autocomplete } from '@mantine/core';
 import BlockImage from './BlockImage';
+import BlockSearchBar from './BlockSearchBar';
 
 export default function BlockScrollArea({blockList}) {
 
-  // data in Autocomplete cannot have duplicate values, better solution possible??
-  const [blockHistory, setBlockHistory] = useState(new Set<string>());
-  const [searchValue, setSearchValue] = useState('');
-  
-  const filteredData = searchValue
-    ? blockList.map(block => block.name).filter(name => name.includes(searchValue))
-    : Array.from(blockHistory);
   return (
     <section id="block-scroll-area">
-    <Autocomplete
-      label="Search"
-      placeholder="grass_block"
-      data={filteredData}
-      value={searchValue}
-      onChange={setSearchValue}
-      limit={5}
-      onOptionSubmit={(value) => {
-        setBlockHistory(new Set([...blockHistory, value]));
-        setSearchValue('');
-      }}
-    />
+      <BlockSearchBar blockList={blockList}/>
       <ScrollArea h={250} type="always" scrollbarSize={12} style={{ padding: '1em'}}>
         <SimpleGrid cols={4} spacing="sm">
         {blockList.map((block, index) => 
---
+++ client/src/components/BlockSearchBar.tsx
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+import { Autocomplete } from '@mantine/core';
+
+export default function BlockSearchBar({blockList, style={}}) {
+  // data in Autocomplete cannot have duplicate values, so use a set, better solution possible??
+  const [blockHistory, setBlockHistory] = useState(new Set<string>());
+  const [searchValue, setSearchValue] = useState('');
+
+  const filteredData = searchValue
+  ? blockList.map(block => block.name).filter(name => name.includes(searchValue))
+    : Array.from(blockHistory);
+  return (
+      <Autocomplete
+        label="Search"
+        placeholder="grass_block"
+        data={filteredData}
+        value={searchValue}
+        onChange={setSearchValue}
+        limit={5}
+        onOptionSubmit={(value) => {
+          setBlockHistory(new Set([...blockHistory, value]));
+          setSearchValue('');
+        }}
+        style={style}
+      />
+  );
+}
