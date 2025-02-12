5a407883f4c112e8d56e7a3796a7df8b7f95db4e
MM1400 <marin.melentii@gmail.com>
2025-02-12 14:04:50 -0500

    Update size of BlockImage ActionIcon

--- client/src/components/BlockImage.tsx
+++ client/src/components/BlockImage.tsx
@@ -6,21 +6,24 @@ import { IconPlus } from '@tabler/icons-react';
 export default function BlockImage({src = null, alt = ''}) {
 
   return (
-    <div className="block-image">
+    <div className="block-image" style={{ position: 'relative'}}>
     <ActionIcon
-      size="xs"
+      size="lg"
+      style={{ position: 'absolute', top: "2em", right: "1em", zIndex: 2 }}
+      aria-label="Add to inventory"
     >
-      <IconPlus />
+      <IconPlus/>
     </ActionIcon>
       <Image
         src={src}
-        h={200}
-        w="auto"
+        height={200}
+        width="auto"
         fit="contain"
         alt={alt}
         fallbackSrc="https://placehold.co/600x400?text=Placeholder"
       />
       {alt}
     </div>
+    
   )
 }
