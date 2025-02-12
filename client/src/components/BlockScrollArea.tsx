e8c2efa2e5531458cf67d65e024b47a16515443a
MM1400 <marin.melentii@gmail.com>
2025-02-12 15:16:06 -0500

    Add search to BlockScrollArea

--- client/src/components/BlockScrollArea.tsx
+++ client/src/components/BlockScrollArea.tsx
@@ -1,15 +1,37 @@
-import React from 'react';
-import { ScrollArea, SimpleGrid } from '@mantine/core';
+import React, {useState} from 'react';
+import { ScrollArea, SimpleGrid, Autocomplete } from '@mantine/core';
 import BlockImage from './BlockImage';
 
 export default function BlockScrollArea({blockList}) {
+
+  // data in Autocomplete cannot have duplicate values, better solution possible??
+  const [blockHistory, setBlockHistory] = useState(new Set<string>());
+  const [searchValue, setSearchValue] = useState('');
+  
+  const filteredData = searchValue
+    ? blockList.map(block => block.name).filter(name => name.includes(searchValue))
+    : Array.from(blockHistory);
   return (
-    <ScrollArea h={250} type="always" scrollbarSize={12}>
-      <SimpleGrid cols={4} spacing="sm">
-      {blockList.map((block, index) => 
-          <BlockImage src={block.src} alt={block.name} key={index}/>
-      )}
-      </SimpleGrid>
-    </ScrollArea>
+    <section id="block-scroll-area">
+    <Autocomplete
+      label="Search"
+      placeholder="grass_block"
+      data={filteredData}
+      value={searchValue}
+      onChange={setSearchValue}
+      limit={5}
+      onOptionSubmit={(value) => {
+        setBlockHistory(new Set([...blockHistory, value]));
+        setSearchValue('');
+      }}
+    />
+      <ScrollArea h={250} type="always" scrollbarSize={12} style={{ padding: '1em'}}>
+        <SimpleGrid cols={4} spacing="sm">
+        {blockList.map((block, index) => 
+            <BlockImage src={block.src} alt={block.name} key={index}/>
+        )}
+        </SimpleGrid>
+      </ScrollArea>
+    </section>
   );
 }
