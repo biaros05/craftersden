33101d338cdf03cfacde6d03ef3b361198ca3de6
MM1400 <marin.melentii@gmail.com>
2025-02-12 14:07:58 -0500

    Adjust height of Image in BlockImage

--- client/src/components/BlockImage.tsx
+++ client/src/components/BlockImage.tsx
@@ -8,17 +8,16 @@ export default function BlockImage({src = null, alt = ''}) {
   return (
     <div className="block-image" style={{ position: 'relative'}}>
     <ActionIcon
-      size="lg"
-      style={{ position: 'absolute', top: "2em", right: "1em", zIndex: 2 }}
+      size="sm"
+      style={{ position: 'absolute', top: "10px", right: "10px", zIndex: 2 }}
       aria-label="Add to inventory"
     >
       <IconPlus/>
     </ActionIcon>
       <Image
         src={src}
-        height={200}
+        height={100}
         width="auto"
-        fit="contain"
         alt={alt}
         fallbackSrc="https://placehold.co/600x400?text=Placeholder"
       />
