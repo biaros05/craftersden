c50eaea8c3d491e81bdb192802a151199f7892d6
MM1400 <marin.melentii@gmail.com>
2025-02-18 21:06:46 -0500

    Fix getBlocks controller using wrong query

--- server/controllers/block.controller.mjs
+++ server/controllers/block.controller.mjs
@@ -5,16 +5,13 @@ export async function getBlocks(req, res, next) {
     const { page = 1, limit = 50 } = req.query;
 
     if (isNaN(page) || isNaN(limit)) {
-      return res.status(400).json({ error: 'Page and limit must be numbers'});
+      return res.status(400).json({ error: 'page and limit parameters must be numbers'});
     }
 
-    // const blocks = await Block.find()
-    //   .limit(limit)
-    //   .skip((page - 1) * limit)
+    const blocks = await Block.find()
+      .limit(limit)
+      .skip((page - 1) * limit)
 
-
-    const blocks = await Block.find({})
-    
     return res.status(200).json({
       blocks: blocks,
       currentPage: page
