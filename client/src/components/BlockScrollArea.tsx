ca9ee7ea219eeb7d2496082e04ce52c5c7d556a6
MM1400 <marin.melentii@gmail.com>
2025-02-20 18:14:37 -0500

    Refactor BlockScrollArea to use useSWR and pass proper data to BlockSearchBar

--- client/src/components/BlockScrollArea.tsx
+++ client/src/components/BlockScrollArea.tsx
@@ -4,6 +4,9 @@ import BlockImage from './BlockImage';
 import BlockSearchBar from './BlockSearchBar';
 import BlockPage from './BlockPage';
 import useSWR from 'swr';
+import { BlockType } from '../../../server/models/BlockType';
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 /**
  * Scrollable area for displaying grid of BlockImage. Contains BlockSearchBar.
@@ -13,28 +16,19 @@ import useSWR from 'swr';
 export default function BlockScrollArea({blockList}) {
 
   const [pageIndex, setPageIndex] = useState<number>(1);
-  const [totalPages, setTotalPages] = useState<number>();
 
-  useEffect(() => {
-    const getTotalPages = async () => {
-      const res = await fetch(`/api/blocks/page-count`);
-      if (res.ok) {
-        const data = await res.json();
-        setTotalPages(data.totalPages);
-      } else {
-        console.error('Failed to fetch page count ', res.status);
-      }
-    }
-    getTotalPages();
-  }, []);
+  const { data: pageCountData, error: pageCountError } = useSWR('/api/blocks/page-count', fetcher);
+  const { data: blockData, error: blockDataError } = useSWR('/api/blocks', fetcher);
+
+  const pageCount = pageCountData?.totalPages;
 
   return (
     <section id="block-scroll-area">
-      <BlockSearchBar blockList={blockList}/>
+      <BlockSearchBar blockList={blockData?.blocks}/>
       {/* Cache next page */}
-      {pageIndex < totalPages &&
+      {pageIndex < pageCount &&
         <div style={{ display: 'none' }}><BlockPage index={ pageIndex + 1 }/></div> }
-      {totalPages && <Pagination total={totalPages} value={pageIndex} onChange={setPageIndex} withPages={true}/>}
+      {pageCount && <Pagination total={pageCount} value={pageIndex} onChange={setPageIndex} withPages={true}/>}
       <ScrollArea h={250} type="always" scrollbarSize={12} style={{ padding: '1em'}}>
         <BlockPage index={pageIndex}/>
       </ScrollArea>
