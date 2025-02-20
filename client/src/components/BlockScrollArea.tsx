7d184a5fc9ea2a1428c7c1be5ba8e37d4b285475
MM1400 <marin.melentii@gmail.com>
2025-02-20 16:52:47 -0500

    Update BlockPage and BlockScrollArea to support pagination

--- client/src/components/BlockScrollArea.tsx
+++ client/src/components/BlockScrollArea.tsx
@@ -1,8 +1,9 @@
-import React, {useState} from 'react';
-import { ScrollArea, SimpleGrid, Autocomplete } from '@mantine/core';
+import React, {useState, useEffect} from 'react';
+import { ScrollArea, Pagination } from '@mantine/core';
 import BlockImage from './BlockImage';
 import BlockSearchBar from './BlockSearchBar';
 import BlockPage from './BlockPage';
+import useSWR from 'swr';
 
 /**
  * Scrollable area for displaying grid of BlockImage. Contains BlockSearchBar.
@@ -11,11 +12,16 @@ import BlockPage from './BlockPage';
  */
 export default function BlockScrollArea({blockList}) {
 
+  const [pageIndex, setPageIndex] = useState<number>(1);
+
   return (
     <section id="block-scroll-area">
       <BlockSearchBar blockList={blockList}/>
+      {/* Cache next page */}
+      <div style={{ display: 'none' }}><BlockPage index={ pageIndex + 1 }/></div>
+      <Pagination total={50} value={pageIndex} onChange={setPageIndex} withPages={true}/>
       <ScrollArea h={250} type="always" scrollbarSize={12} style={{ padding: '1em'}}>
-        <BlockPage index={1}/>
+        <BlockPage index={pageIndex}/>
       </ScrollArea>
     </section>
   );
