c85888a3140f5c40edb6578ce7394436c4fe4810
MM1400 <marin.melentii@gmail.com>
2025-02-20 18:24:17 -0500

    Add clearable option to BlockSearchBar

--- client/src/components/BlockSearchBar.tsx
+++ client/src/components/BlockSearchBar.tsx
@@ -22,13 +22,13 @@ export default function BlockSearchBar({blockList, style={}}) {
   
   function handleOptionSubmit(value: string) {
     setBlockHistory(new Set([...blockHistory, value]));
-    setSearchValue('');
     //TODO: add block to inventory/call func to select it
   }
   return (
       <Autocomplete
         label="Search"
         placeholder="grass_block"
+        clearable={true}
         data={filteredData}
         value={searchValue}
         onChange={setSearchValue}
