50909728f5ed9ed5367f73c23a8991cf51dbff43
Axel Brochu <brochua17@gmail.com>
2025-02-27 22:05:26 -0500

    Add jsdocs to Block component

--- client/src/components/mainUI/Block.tsx
+++ client/src/components/mainUI/Block.tsx
@@ -1,10 +1,14 @@
 import { ThreeElements } from '@react-three/fiber';
 import React, { useState } from 'react';
 
+/**
+ * Creates a mesh with the given props that gets highlighted
+ * when hovered.
+ * @param props React props to be passed to the mesh
+ * @returns A mesh with the given props
+ */
 function Block(props: ThreeElements['mesh']) {
   const [isHovering, setIsHovering] = useState(false);
-
-  // use isHovering to highlight block
 
   return (
     <mesh {...props}
