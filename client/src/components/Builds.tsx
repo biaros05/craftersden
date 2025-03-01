a2dc098c4e992948a9b09d5746c729c3371f363f
Bianca Rossetti <biancaros05@icloud.com>
2025-03-01 13:04:23 -0500

    Fix lint errors

--- client/src/components/Builds.tsx
+++ client/src/components/Builds.tsx
@@ -4,11 +4,12 @@ import { Image } from '@mantine/core';
 import '../styles/builds.css';
 
 // TODO update type to reflect what is stored.
+/* typescript-eslint-disable @typescript-eslint/no-wrapper-object-types */
 type Build = {
   progressPicture: string,
-  description: String,
-  buildJSON: {},
-  isPublished: Boolean,
+  description: string,
+  buildJSON: {}, // eslint-disable-line @typescript-eslint/no-empty-object-type
+  isPublished: boolean,
   thumnails: [],
 }
 
