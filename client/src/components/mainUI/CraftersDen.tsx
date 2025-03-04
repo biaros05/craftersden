f8682194f71f264685ac725a744ce699df11c27c
MM1400 <marin.melentii@gmail.com>
2025-03-04 12:00:35 -0500

    Update BlockType in CraftersDen

--- client/src/components/mainUI/CraftersDen.tsx
+++ client/src/components/mainUI/CraftersDen.tsx
@@ -14,6 +14,7 @@ import { CurrentBlockContext } from '../../context/currentBlockContext';
 
 export type BlockType = {
   id: string,
+  name: string,
   position: [number,number,number],
   geometry: THREE.BufferGeometry,
   textures: THREE.Texture[],
