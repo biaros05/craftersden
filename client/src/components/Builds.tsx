e76f5668d4c760a32950222ca706ff699af06b02
Amy <amynguyen0132@gmail.com>
2025-03-03 20:40:56 -0500

    implement context for build

--- client/src/components/Builds.tsx
+++ client/src/components/Builds.tsx
@@ -1,23 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { createContext } from "react";
+import { useNavigate } from "react-router-dom"
 import '../styles/welcome.css'
 import { Carousel } from '@mantine/carousel';
 import { Image, Text, Box, ActionIcon } from '@mantine/core';
 import '../styles/builds.css';
 
-export default function Builds({builds}) {
-    return (
-      <section className="posts">
-        {
-          builds.map(build => {
-            return <Image
+
+
+export default function Builds({ builds }) {
+  function goToBuild(e) {
+    const navigate = useNavigate();
+    const savedBuild = createContext(e.buildJSON);
+    console.log(savedBuild);
+    navigate('/den');
+  }
+
+  return (
+    <section className="posts">
+      {
+        builds.map(build => {
+          return <Image
             radius="md"
             height={125}
             src={build.progressPicture}
-            onClick={() => console.log(build)}
+            onClick={() => goToBuild(build)}
           />
-          })
-        }
-      </section>
-    )
+        })
+      }
+    </section>
+  )
 }
