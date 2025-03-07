f730307f9164f8c4156d9f5aea1b5f69c88922c1
Amy <amynguyen0132@gmail.com>
2025-03-07 14:42:52 -0500

    Test pass for publish.

--- server/test/publish.test.mjs
+++ server/test/publish.test.mjs
@@ -4,14 +4,15 @@ import { describe, it, before, after } from 'mocha';
 const expect = chai.expect;
 import  app  from '../api.mjs';
 import Sinon from 'sinon';
-import { OAuthService } from '../utils/auth.mjs';
 import mongoose from 'mongoose';
+import { OAuthService } from '../utils/auth.mjs';
 
-let findOneAndUpdateStub;
-let findUserStub;
-let findBuildsStub;
+let findOneAndUpdateStubPublish;
+let findUserStubPublish;
+let findBuildsStubPublish;
 let OAuthClientCreateClientStub;
 let OAuthClientStub;
+let cookie;
 
 const testPostNotPublished = {
   '_id': '9898',
@@ -41,49 +42,52 @@ const initialTestUser = {
   avatar: 'testurl.com'
 };
 
-let cookie;
-
-
 describe('Post publish endpoints', () => {
   before(() => {
-    findOneAndUpdateStub = Sinon.stub(mongoose.Model, 'findOneAndUpdate');
-    findOneAndUpdateStub.resolves(testPostNotPublished);
+    findOneAndUpdateStubPublish = Sinon.stub(mongoose.Model, 'findOneAndUpdate');
+    findOneAndUpdateStubPublish.resolves(testPostNotPublished);
 
-    findUserStub = Sinon.stub(mongoose.Model, 'findOne');
-    findUserStub.resolves({_id: 'bob'});
+    findUserStubPublish = Sinon.stub(mongoose.Model, 'findOne');
+    findUserStubPublish.resolves({_id: 'bob'});
 
-    findBuildsStub = Sinon.stub(mongoose.Model, 'find');
-    findBuildsStub.resolves(testPostIsPublished);
-
+    findBuildsStubPublish = Sinon.stub(mongoose.Model, 'find');
+    findBuildsStubPublish.resolves(testPostIsPublished);
     OAuthClientCreateClientStub = Sinon.stub(OAuthService.prototype, 'createClient');
 
     OAuthClientStub = Sinon.stub(OAuthService.prototype, 'verifyToken');
     OAuthClientStub.resolves(initialTestUser);
   });
 
-  it('Should should publish a build successfully', async () => {
+  it('Should publish a build successfully', async () => {
     const loginResp = await request(app).post('/api/auth').
       send({token: 'faketoken'});
     cookie = loginResp.headers['set-cookie'][0].split(';')[0];
 
     const response = await request(app).
       post('/api/post/publish').
-      field('buildId', '9898');
+      field('buildId', '9898').
+      set('Cookie', cookie);
 
     const query = await request(app).
       get('/api/user/user@test.com/builds').
       set('Cookie', cookie);
 
     expect(response.status).to.equal(200);
-    expect(response.message).to.equal('Build published successfully!');
-    expect(query.body.builds).to.deep.equal([testPostIsPublished]);
+    expect(response.body.message).to.equal('Build published successfully!');
+    expect(query.body.builds).to.deep.equal(testPostIsPublished);
     expect(query.body.message).to.equal('Builds retrieved!');
     return;
   });
 
+  it('should unpublish a build successfully', async () => {
+
+  });
+
   after(() => {
-    findOneAndUpdateStub.restore();
-    findUserStub.restore();
-    findBuildsStub.restore();
+    findOneAndUpdateStubPublish.restore();
+    findUserStubPublish.restore();
+    findBuildsStubPublish.restore();
+    OAuthClientCreateClientStub.restore();
+    OAuthClientStub.restore();
   })
 });
