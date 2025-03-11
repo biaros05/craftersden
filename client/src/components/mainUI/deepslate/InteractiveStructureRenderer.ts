fb9735b2d9ffee7bb5fdb08e28689861c38d672c
Axel Brochu <brochua17@gmail.com>
2025-03-11 13:33:04 -0400

    Add InteractiveStructureRenderer

---
+++ client/src/components/mainUI/deepslate/InteractiveStructureRenderer.ts
@@ -0,0 +1,8 @@
+import { StructureRenderer } from "deepslate";
+import { mat4 } from "gl-matrix";
+
+export default class InteractiveStructureRenderer extends StructureRenderer {
+    public getPerspective(): mat4 {
+        return this.getPerspective();
+    }
+}
