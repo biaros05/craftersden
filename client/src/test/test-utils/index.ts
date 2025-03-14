c75f50597e552e4351ae332956c49dc4b9d28957
MM1400 <marin.melentii@gmail.com>
2025-03-14 18:16:26 -0400

    Refactor render in test-utils to take options for loggedin user

--- client/src/test/test-utils/index.ts
+++ client/src/test/test-utils/index.ts
@@ -1,4 +1,4 @@
-import userEvent from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 export * from '@testing-library/react';
 export { render } from './render';
--- client/src/test/test-utils/render.tsx
+++ client/src/test/test-utils/render.tsx
@@ -4,27 +4,43 @@ import { theme } from '../../theme';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import { AuthProvider } from '../../hooks/useAuth.tsx';
+import { AuthContext, AuthContextType } from '../../hooks/useAuth.tsx';
 import { BuildProvider } from '../../hooks/BuildContext.tsx';
+import { vi } from 'vitest';
+
 
 /**
  * Custom render function that automatically adds necessary provider wrappers.
+ * Starts the auth provider with default values of logged out user.
  *
  * @param {React.ReactNode} ui - The React component to render.
+ * @param {Partial<AuthContextType>} authValue - The value to use for the AuthContext.Provider
  * @returns {RenderResult} The result of the render, including utility functions for testing.
  */
-export function render(ui: React.ReactNode) {
+export function render(ui: React.ReactNode, authValue?: Partial<AuthContextType>) {
+
+  const defaultAuth = {
+    id: '',
+    username: '',
+    email: '',
+    avatar: '',
+    loading: true,
+    login: () => vi.fn(),
+    logout: () => vi.fn(),
+    ...authValue,
+  }
+
   return testingLibraryRender(<>{ui}</>, {
     wrapper: ({ children }: { children: React.ReactNode }) => (
       <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
         <MantineProvider theme={theme}>
-          <AuthProvider >
+          <AuthContext.Provider value={defaultAuth}>
             <BuildProvider>
               <BrowserRouter>
                 {children}
               </BrowserRouter>
             </BuildProvider>
-          </AuthProvider>
+          </AuthContext.Provider>
         </MantineProvider>
       </GoogleOAuthProvider>
     ),
