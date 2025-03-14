c5bb93250f3892c4769ee7192af1aa75cadb576e
MM1400 <marin.melentii@gmail.com>
2025-03-14 11:13:53 -0400

    Refactor Welcome tests to include ProviderWrapper

--- client/src/test/welcome.test.tsx
+++ client/src/test/welcome.test.tsx
@@ -5,39 +5,73 @@ import { http, HttpResponse } from 'msw';
 import  { setupServer } from 'msw/node';
 import { BrowserRouter } from 'react-router-dom';
 import { MantineProvider } from '@mantine/core';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { AuthProvider } from '../hooks/useAuth.tsx';
+import { BuildProvider } from '../hooks/BuildContext.tsx';
+
 
 import Welcome from "../components/Welcome";
 import React from 'react';
 
-// const server = setupServer(
-//   http.get('/api/query', () => {
-//       return HttpResponse.json({user: '1'});
-//     }
-//   )
-// )
+const ProviderWrapper = ({children}) => {
+  return (
+          <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+            <MantineProvider>
+              <AuthProvider >
+                <BuildProvider>
+                  <BrowserRouter>
+                    {children}
+                  </BrowserRouter>
+                </BuildProvider>
+              </AuthProvider>
+            </MantineProvider>
+          </GoogleOAuthProvider>
+  )
+
+}
+
+const server = setupServer(
+  http.get('/api/query', () => {
+      return HttpResponse.json({user: '1'});
+    }
+  )
+)
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: (query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => {},
+  })),
+});
+
+  // If you want to see the DOM structure
+  //screen.debug();
 
   // If you want to see the DOM structure
   //screen.debug();
 
 describe('Welcome', () => {
 
-  // beforeAll(() => server.listen());
+  beforeAll(() => server.listen());
 
-  // afterEach(() => {
-  //   server.resetHandlers();
-  // })
+  afterEach(() => {
+    server.resetHandlers();
+  })
 
-  // afterAll(() => server.close());
+  afterAll(() => server.close());
 
   it('renders login and sign up when logged out', async () => {
-    render(<Welcome />, {wrapper: BrowserRouter});
+    render(<Welcome />, {wrapper: ProviderWrapper});
 
     const loginButton = screen.getByText('Login');
-    const signUpButton = screen.getByText('SignUp');
 
 
     expect(loginButton).toBeInTheDocument();
-    expect(signUpButton).toBeInTheDocument();
 
   });
 });
