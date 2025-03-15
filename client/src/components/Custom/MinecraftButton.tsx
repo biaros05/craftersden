c565d2ec8c0df6006cec3ea175522fcf0eb8a722
Bianca Rossetti <biancaros05@icloud.com>
2025-03-15 00:18:31 -0400

    Minot cleanup

--- client/src/components/Custom/MinecraftButton.tsx
+++ client/src/components/Custom/MinecraftButton.tsx
@@ -4,6 +4,7 @@ import styles from './button.module.css';
 
 /**
  * Creates a custom button component with custom styling
+ * @param {JSON} children - Component's children
  * @returns {React.ReactNode} - custom button component
  */
 export default function MinecraftButton({children}): React.ReactNode { 
