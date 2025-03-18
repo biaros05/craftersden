fdb69918c2bc1d096b9198dce7938d1d9280a7ba
mm1400 <marin.melentii@gmail.com>
2025-03-18 14:27:21 -0400

    Refactor app tests to use renderWithoutWrapper and mock server

--- client/src/test/app.test.tsx
+++ client/src/test/app.test.tsx
@@ -2,25 +2,32 @@ import { describe, it, expect, afterEach, afterAll, beforeAll, vi, beforeEach } 
 import '@testing-library/jest-dom';
 import { http, HttpResponse } from 'msw';
 import  { setupServer } from 'msw/node';
-import { render, screen, userEvent, renderHook, act } from './test-utils';
-
-
-import App from '../main.tsx';
+import {renderWithoutWrapper, screen, userEvent, renderHook, act } from './test-utils';
 import React from 'react';
 
+import App from '../App.tsx';
 
-describe ('App navigation', () => {
+const serverWhileLoggedIn = setupServer(
+  http.get('/api/query', () => {
+      return HttpResponse.json({user: '1'});
+    }
+  )
+)
+describe ('App navigation logged in', () => {
 
-  beforeEach(() => {
-    const root = document.createElement('div');
-    root.id = 'root';
-    document.body.appendChild(root);
+  beforeAll(() => serverWhileLoggedIn.listen());
+  
+  afterEach(() => {
+    serverWhileLoggedIn.resetHandlers();
   })
+  
+  afterAll(() => serverWhileLoggedIn.close());
 
   it ('starts at Welcome page', async () => {
-    render(<App />);
+    renderWithoutWrapper(<App />);
 
     const location = screen.getByTestId('location-display');
     expect(location).toHaveTextContent('/');
   })
+
 })
