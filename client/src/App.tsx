58f8d7c99e122b9039b5965e62b6d50fb6c20ebb
mm1400 <marin.melentii@gmail.com>
2025-03-18 23:07:55 -0400

    Refactor routing structure by moving routes configuration to a separate file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,97 +3,19 @@ import { StrictMode } from 'react';
 import {
   createBrowserRouter,
   RouterProvider,
-  Outlet,
-  useLocation
 } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { AuthProvider } from './hooks/useAuth.tsx';
-import ErrorPage from './error-page.jsx';
-import Header from './components/Header.tsx';
-import Footer from './components/Footer.tsx';
-import Welcome from './components/Welcome.tsx';
-import Login from './components/Login.tsx';
-import Logout from './components/Logout.tsx';
-import ProtectedRoute from './components/ProtectedRoute.tsx';
-import Profile from './components/Profile.tsx';
-import Forum from './components/Forum.tsx';
 import '@mantine/core/styles.css';
 import '@mantine/carousel/styles.css';
-import CraftersDen from './components/mainUI/CraftersDen.jsx';
 import { BuildProvider } from './hooks/BuildContext.tsx';
-import { ToastContainer, Slide } from 'react-toastify';
 import '@mantine/core/styles.css';
 import { MantineProvider} from '@mantine/core';
 import React from 'react';
 import { theme } from './theme';
+import { routesConfig } from './routesconfig.tsx';
 
-export const LocationDisplay = () => {
-  const location = useLocation()
-
-  return <div data-testid="location-display" style={{}}>{location.pathname}</div>
-}
-
-/**
- * Main layout of the app. Renders header
- * and Footer with reactrouter children in
- * between
- * @returns {React.ReactNode} Page parent
- */
-function Main(): React.ReactNode {
-  return <>
-    <Header />
-    <Outlet />
-    <Footer />
-    <ToastContainer
-    position="bottom-right"
-    autoClose={5000}
-    hideProgressBar={false}
-    newestOnTop={false}
-    closeOnClick
-    rtl={false}
-    pauseOnFocusLoss
-    draggable
-    pauseOnHover
-    theme="colored"
-    transition={Slide}
-    />
-    <LocationDisplay/>
-  </>
-}
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Main/>,
-    errorElement: <ErrorPage/>,
-    children: [
-      {
-        path: '',
-        element: <Welcome />,
-      },
-      {
-        path: 'login',
-        element: <ProtectedRoute authed={false} ><Login /></ProtectedRoute>
-      },
-      {
-        path: 'logout',
-        element: <ProtectedRoute authed={true} ><Logout /></ProtectedRoute>
-      },
-      {
-        path: 'forum',
-        element: <ProtectedRoute  authed={true} ><Forum /></ProtectedRoute>
-      },
-      {
-        path: 'profile',
-        element: <ProtectedRoute authed={true} ><Profile /></ProtectedRoute>
-      },
-      {
-        path: 'den',
-        element: <CraftersDen />
-      }
-    ]
-  }
-]);
+const router = createBrowserRouter(routesConfig);
 
 /**
  * The main application component
