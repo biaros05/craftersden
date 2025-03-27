ba24183c2d08ef88d05f0ca3adad4ea045d68ea9
Amy <amynguyen0132@gmail.com>
2025-03-26 21:18:26 -0400

    Add all controllers for Notifications model.

--- server/controllers/inbox.controller.mjs
+++ server/controllers/inbox.controller.mjs
@@ -1,5 +1,12 @@
 import Notification from "../models/Notification";
 
+/**
+ * Adds a notification to user's inbox.
+ * @param {object} req  - The request object.
+ * @param {object} res - The respond object.
+ * @param {*} next - Next
+ * @returns - Response object with status code and message.
+ */
 async function addNotification(req, res, next){
   try{
     const notification = new Notification({
@@ -9,13 +16,38 @@ async function addNotification(req, res, next){
     });
 
     await notification.save();
+    return res.status(200).json({message: 'Notification added'});
   } catch(err){
     err.status = 500;
     next(err);
   }
 };
 
-async function getAllNotifications(req, res, next) {
+/**
+ * This function retrieves all of user's notifications.
+ * @param {object} req  - The request object.
+ * @param {object} res - The respond object.
+ * @param {*} next - Next
+ * @returns - The response object
+ */
+async function getAllNotifications(req, res, next){
+  try{
+    const notifications = await Notification.find({ user: req.body.userId });
+    
+    return res.status(200).json({notifications: notifications});
+  } catch(err){
+    err.status = 500;
+    next(err);
+  }
+}
+
+/**
+ * Retrieves all the notification of user and marks them as read
+ * @param {object} req  - The request object.
+ * @param {object} res - The respond object.
+ * @param {*} next - Next
+ */
+async function readAllNotifications(req, res, next) {
   try {
     const notifications = await Notification.find({ user: req.body.userId });
 
@@ -38,4 +70,40 @@ async function getAllNotifications(req, res, next) {
     next(err);
   }
 };
+
+/**
+ * This function clears all of user's notifications.
+ * @param {object} req  - The request object.
+ * @param {object} res - The respond object.
+ * @param {*} next - Next
+ * @returns - The response object
+ */
+async function clearNotifications(req, res, next) {
+  try {
+    const { userId } = req.body;
+    
+    if (!userId) {
+      return res.status(400).json({ message: "User ID is required" });
+    }
+
+    const result = await Notification.deleteMany({ user: userId });
+
+    if (result.deletedCount === 0) {
+      return res.status(404).json({ message: "No notifications found for this user" });
+    }
+
+    res.json({ message: `Deleted ${result.deletedCount} notifications` });
+
+  } catch (err) {
+    err.status = 500;
+    next(err);
+  }
+}
+
+export {
+  addNotification,
+  getAllNotifications,
+  readAllNotifications,
+  clearNotifications
+}
 
