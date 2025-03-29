98caa6aa30501c15cffe5ba8a4ba49ae55db8b20
Axel Brochu <brochua17@gmail.com>
2025-03-29 01:53:46 -0400

    Implement material counting

--- client/src/components/mainUI/BlockInfo.tsx
+++ client/src/components/mainUI/BlockInfo.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import BlockImage from './BlockImage';
+import { BlockType } from '../../../../server/models/BlockType';
 
 /**
  * Scrollable area for displaying grid of BlockImage. Contains BlockSearchBar.
  * @param {style} style - CSS style object
  * @returns {React.ReactNode} Block scroll area component
  */
-export default function BlockInfo({ block, numberOfBlocks, allowSelectBlock }): React.ReactNode {
+export default function BlockInfo({ block, numberOfBlocks, allowSelectBlock }: {block: BlockType, numberOfBlocks: number, allowSelectBlock: boolean}): React.ReactNode {
 
   return (
     <div id="block-box">
--- client/src/components/mainUI/Materials.tsx
+++ client/src/components/mainUI/Materials.tsx
@@ -3,6 +3,7 @@ import { ScrollArea } from '@mantine/core';
 import { SimpleGrid } from '@mantine/core';
 import BlockInfo from './BlockInfo';
 import { PlacedBlock } from 'deepslate';
+import { BlockType } from '../../../../server/models/BlockType';
 
 /**
  * Scrollable area for displaying grid of BlockImage. Contains BlockSearchBar.
@@ -13,32 +14,39 @@ export default function Materials({ blocks, style = {}}: {blocks: PlacedBlock[];
 
   const scrollViewport = useRef<HTMLDivElement>(null);
   const [blockCounts, setBlockCounts] = useState<Map<string, number>>(new Map());
+  const [blockData, setBlockData] = useState<BlockType[]>();
 
   useEffect(() => {
-    const newBlockCounts = new Map();
-
-    blocks.forEach((block) => {
-      console.log(block)
-      const name = block.state.getName();
-      if (newBlockCounts.has(name)) {
-        newBlockCounts.set(name, newBlockCounts.get(name) + 1);
-      } else {
-        newBlockCounts.set(name, 1);
-      }
-    });
-
-    setBlockCounts(newBlockCounts);
-
-    // fetch all textures + store them... right now the pictures arent getting used anywhere...
-
+    /**
+     * Counts occurences of each block and
+     * fetches their inventory texture
+     */
+    async function getMaterialsData() {
+      const newBlockCounts = new Map();
+      const blockData: Promise<BlockType>[] = [];
+  
+      blocks.forEach((block) => {
+        console.log(block)
+        const name = block.state.getName().path;
+        if (newBlockCounts.has(name)) {
+          newBlockCounts.set(name, newBlockCounts.get(name) + 1);
+        } else {
+          newBlockCounts.set(name, 1);
+          blockData.push(fetch(`/api/block/${name}/image`).
+            then(resp => {
+              if (resp.ok) {
+                return resp.json()
+              }
+            }));
+          }
+      });
+  
+      setBlockCounts(newBlockCounts);
+      setBlockData(await Promise.all(blockData));
+    }
+    
+    getMaterialsData();
   }, [blocks]);
-
-  const blockEntries = blockCounts.entries();
-  const materialList: React.ReactNode[] = [];
-  for (const block of blockEntries) {
-    console.log(block)
-    materialList.push(<BlockInfo block={block} numberOfBlocks={block[1]} allowSelectBlock={false} key={`materials-${block[0]}`}/>)
-  }
 
   return (
     <section id="block-scroll-area" style={style}>
@@ -51,8 +59,7 @@ export default function Materials({ blocks, style = {}}: {blocks: PlacedBlock[];
         style={{ padding: '1em'}} 
         viewportRef={scrollViewport}>
         <SimpleGrid cols={4} spacing="sm">
-          {/* NEED TO USE blockCounts HERE, or else you get duplicates (or turn blocks into a Set) */}
-          {materialList}
+          {blockData?.map(b => {return <BlockInfo block={b} numberOfBlocks={blockCounts.get(b.name)!} allowSelectBlock={false} />})}
         </SimpleGrid>
       </ScrollArea.Autosize>
     </section>
