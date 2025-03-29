19b8097c97dada0cb5f5ebab067107de57ecbef9
Amy <amynguyen0132@gmail.com>
2025-03-29 18:46:53 -0400

    Add groups in search dropdown.

--- client/src/components/ForumSearch.tsx
+++ client/src/components/ForumSearch.tsx
@@ -11,6 +11,15 @@ type Description = {
   description: string;
 };
 
+/**
+ * Search bar for forum, description and username searching
+ * @param {object} props - The component's props
+ * @param {string} props.username - username search
+ * @param {string} props.description - description search
+ * @param {React.Dispatch<React.SetStateAction<string>>} props.setUsername - Sets the username in Forum component
+ * @param {React.Dispatch<React.SetStateAction<string>>} props.setDescription - Sets the description in Forum component
+ * @returns {React.ReactNode} - The forum search bar
+ */
 export function ForumSearch({ username, description, setUsername, setDescription }) {
   const [search, setSearch] = useState("");
   const [users, setUsers] = useState<User[]>([]);
@@ -49,8 +58,8 @@ export function ForumSearch({ username, description, setUsername, setDescription
         setUsers(json.users);
         setDescriptions(json.descriptions);
       } catch (error) {
-        if (error.name !== "AbortError") {
-          console.error("Search failed:", error);
+        if (error.name !== 'AbortError') {
+          console.error('Search failed:', error);
         }
       }
     };
@@ -60,18 +69,22 @@ export function ForumSearch({ username, description, setUsername, setDescription
     return () => controller.abort();
   }, [debouncedSearch]); 
 
+  /**
+   * Handle's the user's click on drop down, sets username or description depending on value
+   * @param {string} selectedValue - The value that user selects from dropdown
+   */
   function handleSubmit(selectedValue: string) {
     const isUser = users.some((user) => user.username === selectedValue);
     const isDescription = descriptions.some((desc) => desc.description === selectedValue);
 
     if (isUser) {
       setUsername(selectedValue);
-      setDescription("");
+      if(description) setDescription('');
     }
 
     if (isDescription) {
       setDescription(selectedValue);
-      setUsername("");
+      if(username) setUsername('');
     }
 
     setSearch("");
@@ -79,34 +92,37 @@ export function ForumSearch({ username, description, setUsername, setDescription
 
   const userData = users.map((user) => ({
     value: user.username,
-    group: "Users",
+    group: 'Users',
     items: (
-      <Group key={user.username} gap="sm">
-        <Avatar src={user.avatar} size={36} radius="xl" />
-        <Text size="sm">{user.username}</Text>
+      <Group key={user.username} gap='sm'>
+        <Avatar src={user.avatar} size={36} radius='xl' />
+        <Text size='sm'>{user.username}</Text>
       </Group>
     ),
   }));
 
   const descriptionData = descriptions.map((desc) => ({
     value: desc.description,
-    group: "Build Descriptions",
+    group: 'Build Descriptions',
   }));
 
   return (
     <Autocomplete
       clearable
-      style={{ width: "400px" }}
-      label="Search for Posts"
-      placeholder="Search username or description..."
+      style={{ width: '400px' }}
+      label='Search for Posts'
+      placeholder='Search username or description...'
       limit={50}
-      data={[...userData, ...descriptionData]}
+      data={[
+        {group: 'Users', items: userData},
+        {group: 'Builds', items: descriptionData}
+      ]}
       onChange={setSearch}
       value={search}
       onOptionSubmit={handleSubmit}
       onClear={() => {
-        setDescription("");
-        setUsername("");
+        setDescription('');
+        setUsername('');
       }}
     />
   );
