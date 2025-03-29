5eca5c79b611042609da5d2275b5bedd56a05b17
mm1400 <marin.melentii@gmail.com>
2025-03-29 16:11:55 -0400

    Refactor InventoryHotbar to allow for keyboard events to trigger storeBlock

--- client/src/components/mainUI/InventoryHotbar.tsx
+++ client/src/components/mainUI/InventoryHotbar.tsx
@@ -1,5 +1,6 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext, useEffect, useRef} from 'react';
 import { InventoryBlockContext } from '../../context/inventoryBlockContext';
+import { CurrentBlockContext } from '../../context/currentBlockContext';
 import InventoryBlock from './InventoryBlock.tsx';
 import {Flex} from '@mantine/core';
 /**
@@ -11,9 +12,40 @@ export default function InventoryHotbar({}) {
     inventoryBlocks
   } = useContext(InventoryBlockContext);
 
+  const {
+    storeBlock
+  } = useContext(CurrentBlockContext);
+
+  const keyboardControlsRef = useRef<(e: KeyboardEvent) => void>(null);
+
+  /**
+   * Event handler to cycle through different states
+   * @param {KeyboardEvent} e - Event object 
+   */
+  function keyboardControls(e: KeyboardEvent) {
+    const keyPressed = Number(e.key);
+    if (isNaN(keyPressed)) {
+      return;
+    }
+    const selectedBlock = inventoryBlocks[keyPressed - 1];
+    if (selectedBlock) {
+      storeBlock(selectedBlock);
+    }
+  }
+
   useEffect(() => {
-    console.log(inventoryBlocks);
-  })
+    keyboardControlsRef.current = keyboardControls;
+  }, [inventoryBlocks]);
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (keyboardControlsRef.current) {
+        keyboardControlsRef.current(e);
+      }
+    }
+    document.addEventListener('keydown', handler);
+    return () => document.removeEventListener('keydown', handler);
+  }, []);
 
   return (
     <Flex 
@@ -21,7 +53,11 @@ export default function InventoryHotbar({}) {
       direction="row"
       >
       {inventoryBlocks.map((block, index) => 
-        <InventoryBlock block={block} key={index}/>
+        <InventoryBlock 
+          block={block} 
+          key={index} 
+          onClick={() => storeBlock(block)}
+          />
       )}
     </Flex>
   )
